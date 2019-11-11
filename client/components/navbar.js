e0ab93aaf643f2e65a361687838ff5a93dd6fd97
Michael Mcmanus <mikemcmanus95@gmail.com>
2019-11-11 17:36:00 -0500

    removed react-bootstrap

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
-import Nav from 'react-bootstrap/Nav'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
