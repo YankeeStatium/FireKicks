d28bd990ae83050fdb06b43174d28a3e3c85fb39
Michael Mcmanus <mikemcmanus95@gmail.com>
2019-11-13 11:10:55 -0500

    bugfixes

--- constants/frontend.js
+++ constants/frontend.js
@@ -1,6 +1,6 @@
 const FRONTEND_DEV_URLS = ['http://localhost:8080']
 const FRONTEND_PROD_URLS = [
-  'https://www.yourdomain.com',
+  'https://firekicks.herokuapp.com',
   'https://yourdomain.com'
 ]
 module.exports =
